215969fc22a811ff31ba112a1c6cf8877e1a3da3
OksanaSanag <39303694+OksanaSanag@users.noreply.github.com>
2019-06-29 17:25:50 +0300

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,7 +1,7 @@
 //Write a function which returns the smallest of three numbers.
 
 function min(a, b, c) {
-    console.log('numbers', a, b, c);
+    //console.log('numbers', a, b, c);
     return (a < b && a < c) ? a : (a < b && a > c) ? c : b ;
     }
 
