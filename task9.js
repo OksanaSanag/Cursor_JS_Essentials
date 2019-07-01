b4c76e3c722f03554d828f313c8b220b93dc206a
OksanaSanag <39303694+OksanaSanag@users.noreply.github.com>
2019-07-01 13:10:33 +0300

    Update task9.js

--- task9.js
+++ task9.js
@@ -3,11 +3,15 @@
 function toUppercase(str) { 
    let str2 = '';
    for (let i = 0; i < str.length; i++) {
-      str2 = str2 + String.fromCharCode(str.charCodeAt(i) - 32);
+     if (str.charCodeAt(i) > 96 && str.charCodeAt(i) < 123) {
+       str2 = str2 + String.fromCharCode(str.charCodeAt(i) - 32);
+     }
+     else str2 = str2 + str[i];
    };
    
    console.log(str2);
    return str2;
 } 
 
-toUppercase('abc') // 'ABC' 
+toUppercase('abc');
+toUppercase('abc 12а %^&*@#$!df fg 56h#$');
